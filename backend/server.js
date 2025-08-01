adb7a419ac5334cbd0db3ca6cec05f2793cb2298
prakash <prakashvadakadu8@gmail.com>
2025-08-01 21:08:57 +0530

    server error  fix

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,15 @@ import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
-app.use(express.json());
+
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
+
 const allowedOrigins = [
   "http://localhost:5173",
   "https://whatsapp-client-nine.vercel.app",
@@ -23,11 +27,12 @@ app.use(
 );
 connectDb();
 
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
-
 app.get("/", (req, res) => {
   res.send("Hii..");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).json({ message: "Route not found" });
 });
 
 app.use("/api/auth", authRoute);
